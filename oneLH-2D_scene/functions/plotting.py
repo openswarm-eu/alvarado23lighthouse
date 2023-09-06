3abafcf6316e8e273a0b29d74c8ef0e5d146a934
Said Alvarado <said_alvarado@hotmail.com>
2023-09-06 19:42:40 +0200

    # 3 Add histogram plot function

--- oneLH-2D_scene/functions/plotting.py
+++ oneLH-2D_scene/functions/plotting.py
@@ -1,4 +1,5 @@
 import numpy as np
+import seaborn as sns
 
 # Fix to avoid Type 3 fonts on the figures
 # http://phyletica.org/matplotlib-fonts/
@@ -11,6 +12,29 @@ from matplotlib.gridspec import GridSpec
 
 def plot_trajectory_and_error(lh2_data, camera_data, error, start_time, end_time):
     """
+    Plots a superposition of the ground truth trajectory and estimated LH2 trajectory.
+    As well as a separate subplot with the error.
+
+    Parameters
+    ----------
+    lh2_data : Dict
+        Dictionary of numpy arrays with the LH2 data.
+        values:
+            'x': array, float (N,)
+                X axis data
+            'y': array, float (N,)
+                Y axis data
+            'time': array, int (N,)
+                t in unix epoch (microseconds)
+    camera_data : Dict
+        Same as lh2_data, but with the camera data
+    error : array, float, shape (N,2)
+        Euclidean error between lh2_data and camera_data
+    start_time : float
+        start time for the plot (in seconds)
+    end_time : float
+        end time for the plot (in seconds)
+
     """
 
     # Find the indexes of the start and end times for the plots
@@ -64,4 +88,49 @@ def plot_trajectory_and_error(lh2_data, camera_data, error, start_time, end_time
 
     plt.savefig('Result-A-1lh_2d-example.pdf')
 
+    plt.show()
+
+
+def plot_error_histogram(errors):
+    """
+
+    Plot a histogram of the provided distance errors
+
+    Parameters
+    ----------
+    errors : array_like, float, shape (N,2)
+        Array of euclidean error
+
+    """
+
+    # Plot the results
+    fig = plt.figure(layout="constrained", figsize=(5,4))
+    gs = GridSpec(3, 3, figure = fig)
+    # Define individual subplots
+    hist_ax    = fig.add_subplot(gs[0:3, 0:3])
+    axs = [hist_ax,]
+
+    # Sea-born KDE histogram plot
+    sns.histplot(data=errors,  bins=50, ax=hist_ax, linewidth=0, color="xkcd:baby blue")
+    hist_ax.set_xlim((0, 22))
+    ax2 = hist_ax.twinx()
+    sns.kdeplot(data=errors, ax=ax2, label="density", color="xkcd:black", linewidth=1, linestyle='--')
+
+    # Plot the mean line
+    hist_ax.axvline(x=errors.mean(), color='xkcd:red', label="Mean")
+    # Trick to get the legend unified between the TwinX plots
+    hist_ax.plot([], [], color="xkcd:black", linestyle='--', label = 'density')
+
+    # Add labels and grids
+    for ax in axs:
+        ax.legend()
+    
+    # Configure the plot options
+    xticks_locs = np.linspace(0, 20, 5)  # 5 x-ticks from 0 to 10
+    hist_ax.set_xticks(xticks_locs)
+    hist_ax.set_xlabel('Distance Error [mm]')
+    hist_ax.set_ylabel('Measurements')
+
+    # Save and show figure
+    plt.savefig('Result-B-1lh_2d-histogram.pdf')
     plt.show()
